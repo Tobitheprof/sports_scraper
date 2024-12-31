f89aac2245e3b90646c574446e7f9b03cf52f480
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2024-12-31 19:59:22 +0100

    chore: fixed date parsing logic

--- sports_scraper/spiders/metro_spider.py
+++ sports_scraper/spiders/metro_spider.py
@@ -19,11 +19,15 @@ class MetroSpiderSpider(scrapy.Spider):
         item['title'] = self.clean_text(response.css("h1.article__title::text").get())
         item['url'] = response.url
         item['author'] = self.clean_text(response.css("a.author-name::text").get())
-        item['date'] = self.clean_text(response.css("span.article__published::text").get())
+
+        date_raw = response.css("div.article__date span.article__published *::text").getall()
+        item['date'] = self.clean_text(" ".join([text.strip() for text in date_raw if text.strip()]).replace("Published", ""))
+
         item['body'] = self.clean_body(response.css("div.article__content__inner p::text").getall())
         item['source'] = "Metro UK"
 
         yield item
+
 
     def clean_text(self, text):
         """Clean and normalize text."""
